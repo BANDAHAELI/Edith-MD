a4abb313decf4dbc24aa2ac55a27694db500a54e
BANDAHAELI <ali.khan.urf.bandali@gmail.com>
2025-07-31 06:45:59 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "",
+  START_ING: process.env.START_IMG || "",
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
   AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
   AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
